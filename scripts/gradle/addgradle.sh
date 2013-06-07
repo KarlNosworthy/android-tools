bc5a0239796be5e3d6610871be76c66ccaad1cf8
Karl Nosworthy <k.nosworthy@gmail.com>
2013-06-07 07:48:18 +0100

    Added in android library project support.  Made both scripts more verbose.

--- scripts/gradle/addgradle.sh
+++ scripts/gradle/addgradle.sh
@@ -10,11 +10,11 @@
 # A simple utility to add gradle structure and initial configuration
 # to an existing android project.
 #
-# addgradle.sh <path> <compile sdk version>
+# addgradle.sh <path> <compile sdk version> [library project <Y/N>] [gradle version] [android plugin version]
 
 if [ "$#" -lt "2" ]
 	then
-	echo "addgradle.sh <path> <compile sdk version> [gradle version]"
+	echo "addgradle.sh <path> <compile sdk version> [library project <Y/N>] [gradle version] [android plugin version] "
 	exit
 fi
 
@@ -22,16 +22,39 @@ fi
 TARGET_PROJECT_PATH="$1"
 PROJECT_COMPILE_SDK_VERSION="$2"
 
-if [ -z "$3"]
+if [ -z "$3" ]
+	then
+	PROJECT_IS_LIBRARY="N"
+else
+	PROJECT_IS_LIBRARY="Y"
+fi
+
+if [ -z "$4"]
 	then
 	GRADLE_VERSION="1.6"
 else
-	GRADLE_VERSION="$3"
+	GRADLE_VERSION="$4"
 fi
+
+if [ -z "$5" ]
+	then
+	ANDROID_PLUGIN_VERSION="0.4.2"
+else
+	ANDROID_PLUGIN_VERSION="$5" 
+fi
+
+#
+echo -ne "\n"
+echo " Gradle configuration - Version:                ${GRADLE_VERSION}"
+echo -e "                        Android Plugin Version: ${ANDROID_PLUGIN_VERSION}\n"
+echo -n "   > Adding gradle support to project"
 
 # Create the gradle main directory structure
 mkdir -p ${TARGET_PROJECT_PATH}/src/main/java
 mkdir -p ${TARGET_PROJECT_PATH}/src/main/res
+
+echo -e "\t\t\t\t\t [done]"
+echo -n "   > Generating build.gradle"
 
 # Create the build.gradle file
 if [ ! -f "${TARGET_PROJECT_PATH}/build.gradle" ];
@@ -42,13 +65,19 @@ if [ ! -f "${TARGET_PROJECT_PATH}/build.gradle" ];
 	echo -e "\t}\n" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo "" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo -e "\tdependencies {" >> ${TARGET_PROJECT_PATH}/build.gradle
-	echo -e "\t\tclasspath 'com.android.tools.build:gradle:0.4.2'" >> ${TARGET_PROJECT_PATH}/build.gradle
+	echo -e "\t\tclasspath 'com.android.tools.build:gradle:${ANDROID_PLUGIN_VERSION}'" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo -e "\t}" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo -e "}" >> ${TARGET_PROJECT_PATH}/build.gradle
-	echo -e "task wrapper(type: Wrapper) {\n\tgradleVersion = '${GRADLE_VERSION}'\n}" >> ${TARGET_PROJECT_PATH}/build.gradle	
-	echo -e "apply plugin: 'android'\n" >> ${TARGET_PROJECT_PATH}/build.gradle
+	echo -e "task wrapper(type: Wrapper) {\n\tgradleVersion = '${GRADLE_VERSION}'\n}" >> ${TARGET_PROJECT_PATH}/build.gradle
+	if [ "${PROJECT_IS_LIBRARY}" == "Y" ]
+		then
+		echo -e "apply plugin: 'android-library'\n" >> ${TARGET_PROJECT_PATH}/build.gradle
+	else
+		echo -e "apply plugin: 'android'\n" >> ${TARGET_PROJECT_PATH}/build.gradle
+	fi
 	echo "android {" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo -e "\tcompileSdkVersion ${PROJECT_COMPILE_SDK_VERSION}" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo -e "\tbuildToolsVersion \"${PROJECT_COMPILE_SDK_VERSION}\"" >> ${TARGET_PROJECT_PATH}/build.gradle
 	echo "}" >> ${TARGET_PROJECT_PATH}/build.gradle
 fi
+echo -e "\t\t\t\t\t\t [done]"
--- scripts/gradle/newproject.sh
+++ scripts/gradle/newproject.sh
@@ -11,11 +11,11 @@
 # the correct directory structure and default AndroidManifest, strings.xml
 # and gradle.build files.
 #
-# newproject.sh <project name> <path> <root package name> [compile sdk version] [main activity name]
+# newproject.sh <project name> <path> <root package name> [compile sdk version] [library project <Y/N>] [main activity name]
 
 if [ "$#" -lt "3" ]
 	then
-	echo "newproject.sh <project name> <path> <root package name> [compile sdk version] [main activity name]"
+	echo "newproject.sh <project name> <path> <root package name> [compile sdk version] [library project <Y/N>] [main activity name]"
 	exit
 fi
 
@@ -27,29 +27,46 @@ PROJECT_PACKAGE_NAME="$3"
 # check if a target/build sdk version has been passed in
 if [ -n "$4" ]
 	then
-	PROJECT_COMPILE_SDK_VERSION="17"
-else
 	PROJECT_COMPILE_SDK_VERSION="$4"
+else
+	PROJECT_COMPILE_SDK_VERSION="17"
 fi
 
-# check if a main activity name has been specified
-if [ -n "$5" ]
+#
+if [  "$5" == "Y" ]
 	then
-	PROJECT_MAIN_ACTIVITY_NAME="$5"
+		PROJECT_IS_LIBRARY="Y"
+	else
+		PROJECT_IS_LIBRARY="N"
+	fi
+
+# check if a main activity name has been specified
+if [ -n "$6" ]
+	then
+	PROJECT_MAIN_ACTIVITY_NAME="$6"
 fi
+
 
 # Set target root project path
 TARGET_PROJECT_PATH=${PROJECT_PATH}/${PROJECT_NAME}
 
 # Create project directories at specified path
+echo ""
+echo " Creating Project: ${PROJECT_NAME}"
 mkdir -p ${TARGET_PROJECT_PATH}/src
 mkdir -p ${TARGET_PROJECT_PATH}/src/main/res/values
 
 # Create the gradle structure and configuration
 CWD=`pwd`
-bash ${CWD}/addgradle.sh "${TARGET_PROJECT_PATH}" "${PROJECT_COMPILE_SDK_VERSION}"
+bash ${CWD}/addgradle.sh "${TARGET_PROJECT_PATH}" "${PROJECT_COMPILE_SDK_VERSION}" "${PROJECT_IS_LIBRARY}"
 
 # Create a default manifest file
+echo -e "\n"
+echo " Android configuration - Target SDK Version:  ${PROJECT_COMPILE_SDK_VERSION}"
+echo "                         Root Package Name :  ${PROJECT_PACKAGE_NAME}"
+echo -e "                         Main Activity Name:  ${PROJECT_MAIN_ACTIVITY_NAME}\n"
+
+echo -n "   > Generating AndroidManifest.xml"
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>" >> ${TARGET_PROJECT_PATH}/src/main/AndroidManifest.xml
 echo "<manifest xmlns:android=\"http://schemas.android.com/apk/res/android\"" >> ${TARGET_PROJECT_PATH}/src/main/AndroidManifest.xml
 echo -e "\tpackage=\"${PROJECT_PACKAGE_NAME}\"\n\tandroid:versionCode=\"1\"\n\tandroid:versionName=\"1.0\" >" >> ${TARGET_PROJECT_PATH}/src/main/AndroidManifest.xml
@@ -65,9 +82,12 @@ if [ -n "$PROJECT_MAIN_ACTIVITY_NAME" ]
 		echo -e "\t</application>" >> ${TARGET_PROJECT_PATH}/src/main/AndroidManifest.xml
 fi
 echo "</manifest>" >> ${TARGET_PROJECT_PATH}/src/main/AndroidManifest.xml
+echo -e "\t\t\t\t\t [done]"
 
 # Create the strings resources file (default)
+echo -n "   > Generating default strings.xml"
 echo "<?xml version=\"1.0\" encoding=\"utf-8\">"  >> ${TARGET_PROJECT_PATH}/src/main/res/values/strings.xml
 echo "<resources>"  >> ${TARGET_PROJECT_PATH}/src/main/res/values/strings.xml
 echo -e "\t<string name=\"app_name\">${PROJECT_NAME}</string>"  >> ${TARGET_PROJECT_PATH}/src/main/res/values/strings.xml
 echo "</resources>"  >> ${TARGET_PROJECT_PATH}/src/main/res/values/strings.xml
+echo -e "\t\t\t\t\t [done]\n"
